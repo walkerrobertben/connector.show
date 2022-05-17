b834c8050e810dd25176a6752d383a728318b58a
Ben <walkerrobertben.signup@gmail.com>
2022-05-17 18:25:45 +0100

    Implemented back button

--- v1.js
+++ v1.js
@@ -26,9 +26,19 @@ const elem_imageFront = document.getElementById("connector_image_front");
 const elem_dimWidth = document.getElementById("connector_dimension_width");
 const elem_dimHeight = document.getElementById("connector_dimension_height");
 
+
 //Render loaded information
 elem_pageTitle.innerHTML = data_name;
 elem_connName.innerHTML = data_name;
 elem_imageFront.src = directory + "/front_optimised.svg";
 elem_dimWidth.innerHTML = data_width + data_dimUnit;
 elem_dimHeight.innerHTML = data_height + data_dimUnit;
+
+//Show back button if needed
+if (queryParams.get("back") == "true") {
+    const button = document.getElementById("back_button")
+    button.classList.remove("hidden");
+    button.onclick = function() {
+        history.back();
+    }
+}
