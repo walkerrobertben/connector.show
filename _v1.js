3f20d2d328fe8612bd52948a8e098961568c894b
Ben <walkerrobertben.signup@gmail.com>
2022-05-17 16:52:18 +0100

    Moved initial work into repo

---
+++ _v1.js
@@ -0,0 +1,34 @@
+//Parse get-parameters
+const queryString = window.location.search;
+const queryParams = new URLSearchParams(queryString);
+const connector = queryParams.get("connector");
+const directory = "_v1/" + connector;
+
+//Perform HTTP request to load data xml
+const loadDataRequest = new XMLHttpRequest();
+loadDataRequest.open("GET", directory + "/data.xml", false);
+loadDataRequest.setRequestHeader("Content-Type", "text/xml");
+loadDataRequest.send();
+
+//Parse data XML
+const data = new DOMParser().parseFromString(loadDataRequest.responseText, "text/xml");
+const data_name = data.getElementsByTagName("name")[0].innerHTML;
+const data_dimensions = data.getElementsByTagName("dimensions")[0];
+
+const data_width = data_dimensions.getElementsByTagName("width")[0].innerHTML;
+const data_height = data_dimensions.getElementsByTagName("height")[0].innerHTML;
+const data_dimUnit = data_dimensions.getElementsByTagName("units")[0].innerHTML;
+
+//Find page elements
+const elem_pageTitle = document.getElementsByTagName("title")[0];
+const elem_connName = document.getElementById("connector_name");
+const elem_imageFront = document.getElementById("connector_image_front");
+const elem_dimWidth = document.getElementById("connector_dimension_width");
+const elem_dimHeight = document.getElementById("connector_dimension_height");
+
+//Render loaded information
+elem_pageTitle.innerHTML = data_name;
+elem_connName.innerHTML = data_name;
+elem_imageFront.src = directory + "/front_optimised.svg";
+elem_dimWidth.innerHTML = data_width + data_dimUnit;
+elem_dimHeight.innerHTML = data_height + data_dimUnit;
